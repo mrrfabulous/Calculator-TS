6e161a7010664531240b38fcbc75a7395fba8dcd
Joseph Innocent <65666788+mrrfabulous@users.noreply.github.com>
2024-02-10 15:44:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,19 +4,41 @@ const upperScreen = document.querySelector(".upper-screen");
 const lowerScreen = document.querySelector(".lower-screen");
 let answer = document.getElementById("answer");
 let result;
+const evaluate = () => {
+    try {
+        result = eval(upperScreen.innerHTML);
+        lowerScreen.innerHTML = result;
+        upperScreen.innerHTML = "";
+    }
+    catch (error) {
+        lowerScreen.innerHTML = "Error";
+    }
+};
+const typingDisplay = () => {
+    result = eval(upperScreen.innerHTML);
+    lowerScreen.innerHTML = result;
+};
 const handleClick = (button) => {
     if (!isNaN(parseFloat(button.innerText))) {
         upperScreen.innerHTML += button.innerText;
+        typingDisplay();
     }
     else if (button.innerText == "C") {
         upperScreen.innerHTML = "";
         lowerScreen.innerHTML = "";
     }
-    else if (button.innerText == "BS") {
+    else if (button.innerText == "backspace") {
         upperScreen.innerHTML = upperScreen.innerHTML.slice(0, -1);
+        if (upperScreen.innerHTML != "") {
+            typingDisplay();
+        }
+        else {
+            lowerScreen.innerText = "";
+        }
     }
     else if (button.innerText == "/" || button.innerText == "*" || button.innerText == "-" || button.innerText == "+") {
         upperScreen.innerHTML += " " + button.innerHTML + " ";
+        typingDisplay();
     }
     else if (button.innerText === ".") {
         if (!upperScreen.innerHTML.includes(".")) {
@@ -28,18 +50,11 @@ const handleClick = (button) => {
             upperScreen.innerHTML += button.innerHTML;
         }
     }
-    else if (button.innerText == "ans.") {
-        upperScreen.innerHTML += result;
-    }
+    // else if (button.innerText == "ans.") {
+    //     upperScreen.innerHTML += result
+    // }
     else if (button.innerText == "=") {
-        try {
-            result = eval(upperScreen.innerHTML);
-            lowerScreen.innerHTML = result;
-            upperScreen.innerHTML = "";
-        }
-        catch (error) {
-            lowerScreen.innerHTML = "Error";
-        }
+        evaluate();
     }
 };
 buttons.forEach(button => {
